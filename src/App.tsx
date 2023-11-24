0114fa1ab4d2130c9a4c03d14732396c61d45876
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-11-24 00:49:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
+import { Card } from './componentes/card/card';
+import { useFoodData } from './hooks/useFoodData';
 
-function App() {
-  const data = FoodData[] = []; 
+    function App() {
+  const { data } = useFoodData();
 
   return (
       <div className="container">
        <h1>Cardápio</h1>
        <div className='card-grid'>
-        {data.map(foodData => 
-          <Card 
+        {data?.map(foodData =>
+          <Card
             price={foodData.price} 
             title={foodData.title} 
             image={foodData.image}
